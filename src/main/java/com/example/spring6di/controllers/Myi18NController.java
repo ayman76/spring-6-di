c8feccbbde58cab97c13b40a8538cfebfcc1598c
Ayman Mohamed <ayman.mohamed@tildetech.ae>
2024-06-02 16:14:10 +0300

    Profiles

---
+++ src/main/java/com/example/spring6di/controllers/Myi18NController.java
@@ -0,0 +1,18 @@
+package com.example.spring6di.controllers;
+
+import com.example.spring6di.service.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Myi18NController {
+    private final GreetingService greetingService;
+
+    public Myi18NController(@Qualifier("i18NService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/com/example/spring6di/controllers/Myi18NControllerTestEN.java
@@ -0,0 +1,22 @@
+package com.example.spring6di.controllers;
+
+import com.example.spring6di.service.GreetingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("EN")
+@SpringBootTest
+class Myi18NControllerTestEN {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
